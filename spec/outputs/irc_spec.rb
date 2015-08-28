0a304f2d54298c5c4cf18b9a638f9b8ebf183a2a
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-08-28 16:49:51 +0200

    add basic test for the plugin
    Fixes #5

--- spec/outputs/irc_spec.rb
+++ spec/outputs/irc_spec.rb
@@ -1,1 +1,15 @@
-require "logstash/devutils/rspec/spec_helper"
+# encoding: utf-8
+require_relative "../spec_helper"
+
+describe LogStash::Outputs::Irc do
+
+  let(:host) { "localhost" }
+  let(:port) { rand(2048)+1024 }
+
+  let(:channels) { ["logstash"] }
+  it "should register without errors" do
+    plugin = LogStash::Plugin.lookup("output", "irc").new("host" => host, "port" => port, "channels" => channels)
+    expect { plugin.register }.to_not raise_error
+  end
+
+end
